5b14076736662e63134da03b3e919359ab1e43d4
vikolga <ov.reatch@gmail.com>
2023-08-20 11:16:49 +0300

    третье ревью

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'default')
 
-DEBUG = os.getenv('DEBUG', 'False').lower() == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['158.160.71.236', '127.0.0.1', 'localhost', 'kittygramov.hopto.org']
 
